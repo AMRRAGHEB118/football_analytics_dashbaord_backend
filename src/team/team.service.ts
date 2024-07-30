aee336d586d4ff1f0c19d2641747bb3a1828dab8
Makarona10 <ahmedneyssi@gmail.com>
2024-07-31 00:13:53 +0300

    Build -> Reload team API built

--- src/team/team.service.ts
+++ src/team/team.service.ts
@@ -1,4 +1,45 @@
 import { Injectable } from '@nestjs/common';
+import { Team } from './schema/team.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { DataImportService } from 'src/services/dataImport/data.import.service';
 
 @Injectable()
-export class TeamService {}
+export class TeamService {
+  constructor(@InjectModel(Team.name) private teamModel: Model<Team>,
+    private readonly dataImport: DataImportService) { }
+
+  async findOne(id: number, seasonId: number) {
+    const team = await this.teamModel.findOne({ id })
+      .populate({ path: 'statistics', match: { seasonId: seasonId } }).exec();
+
+    if (!team) {
+      return {
+        "message": "Team Not Found!",
+        "status": 404,
+        "data": []
+      }
+    }
+    return {
+      "message": "Team returned successfully",
+      "status": 200,
+      "data": [team]
+    };
+  }
+
+  async reloadTeam(id: number) {
+    const reloadedTeam = await this.dataImport.importTeam(id);
+    if (reloadedTeam)
+      return {
+        "message": "Team reloaded successfully",
+        "status": 200,
+        "data": [reloadedTeam],
+      }
+    else return {
+      "message": "Team not found ,Please make sure of the id",
+      "status": 404,
+      "data": [reloadedTeam],
+    }
+  }
+
+}
