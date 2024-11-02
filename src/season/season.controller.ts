461230fbfe9c9be94dd4f96b6db507e8c6acf1ed
Makarona10 <ahmedneyssi@gmail.com>
2024-11-02 21:48:15 +0200

    Build -> Added get seasons API

--- src/season/season.controller.ts
+++ src/season/season.controller.ts
@@ -1,12 +1,106 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { SeasonService } from './season.service';
+import { Response } from 'express';
+import { LoggerService } from 'src/services/logger/logger.service';
+import { LoggerModule } from 'src/services/logger/logger.schema';
+import _Response from 'src/types';
 
-@Controller('season')
+@Controller('seasons')
 export class SeasonController {
-  constructor( private seasonService: SeasonService) {}
+  constructor(
+    private seasonService: SeasonService,
+    private readonly loggerService: LoggerService,
+  ) {}
+
+  @Get('fetch')
+  async fetchSeasons(@Res() response: Response) {
+    const s: number = performance.now();
+    const result: _Response = await this.seasonService.fetchSeasons();
+    let duration: number = performance.now() - s;
+    duration = parseFloat(duration.toFixed(2));
+    if (result.status_code == 200) {
+      this.loggerService.logInfo(
+        `Seasons fetched successfully`,
+        '/seasons/fetch',
+        'GET',
+        200,
+        LoggerModule.SEASON,
+        duration,
+      );
+      return response.status(200).send({
+        message: 'Seaons fetched successfully',
+        status_code: 200,
+        data: result.data,
+      });
+    }
+
+    this.loggerService.logError(
+      `Server error while fetching seasons`,
+      '/seasons/fetch',
+      'GET',
+      500,
+      LoggerModule.SEASON,
+      duration,
+    );
+    return response.status(500).send({
+      message: 'Failed to fetch seasons, try again later.',
+      status_code: 500,
+      data: [],
+    });
+  }
 
   @Get()
-  async getSeasons() {
-    return this.seasonService.getSeasons()
+  async getSeasons(@Res() response: Response) {
+    const s: number = performance.now();
+    try {
+      const seasons = await this.seasonService.getSeasons();
+      let duration: number = performance.now() - s;
+      duration = parseFloat(duration.toFixed(2));
+      if (!seasons) {
+        this.loggerService.logError(
+          `No seasons found`,
+          'seasons/',
+          'GET',
+          404,
+          LoggerModule.SEASON,
+          duration,
+        );
+        return response.status(404).send({
+          message: 'No seasons found.',
+          status_code: 404,
+          data: [],
+        });
+      }
+      this.loggerService.logInfo(
+        `Seasons retrieved successfully`,
+        'seasons/',
+        'GET',
+        200,
+        LoggerModule.SEASON,
+        duration,
+      );
+      return response.status(200).send({
+        message: 'Seasons retrieved successfully.',
+        status_code: 200,
+        data: seasons,
+      });
+    } catch (error) {
+      let duration: number = performance.now() - s;
+      duration = parseFloat(duration.toFixed(2));
+      this.loggerService.logError(
+        `Failed to get seasons`,
+        'seasons/',
+        'GET',
+        500,
+        LoggerModule.SEASON,
+        duration,
+        error
+      );
+      return response.status(500).send({
+        message: 'Failed to get seasons.',
+        status_code: 500,
+        data: [],
+      });
+    }
   }
 }
