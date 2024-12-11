727ff334924d811e043959d21c4e541d6fdaf521
Makarona10 <ahmedneyssi@gmail.com>
2024-12-11 20:45:01 +0200

    Enhance -> added a new function to unify the response all over the app

--- src/types.ts
+++ src/types.ts
@@ -8,6 +8,18 @@ export type _ServerResponse = {
   message: string;
   status_code: number;
   data: any[];
+};
+
+export function resObj(
+  message: string,
+  status_code: number,
+  data: Array<any>,
+): _ServerResponse {
+  return {
+    message,
+    status_code,
+    data,
+  };
 }
 
 export default _Response;
